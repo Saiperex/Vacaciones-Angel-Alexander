d4e2c1badd623c751e2d989a8780b91e6b86b275
Xavier <xaviramos668@gmail.com>
2022-10-20 23:24:03 -0300

    Agregando el script de transicion

---
+++ script/transition.js
@@ -0,0 +1,20 @@
+var imagenes=["7cascadas.jpg","terones.jpg","zapato.jpg","ongamira.jpg"]
+var fondo=document.getElementById("fondo");
+var index=0;
+function cambiarFondo()
+{
+    
+    if(index<=2)
+    {
+        index=index+1;
+    }
+    else
+    {
+        index=0;
+    }
+    var url="url('../imagenes/"+imagenes[index]+"')"
+    fondo.style.backgroundImage=url;
+    fondo.style.transition="1s all";
+    tiempo=window.setTimeout('cambiarFondo()',5000)
+}
+cambiarFondo();
